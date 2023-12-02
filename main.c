7118bb5f8b0fc700df5decd636cfed630069ae93
dmzotov1986 <152577337+dmzotov1986@users.noreply.github.com>
2023-12-02 11:02:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include"settings.h"
 void print_help(){
-	printf("It's program for calculating many different parameters of chess/draughts match time: p - maximum match (of given count of moves played) time for both players (in minutes), t - maximum time for match given to only one player (in minutes), n - number of moves played in match by one winner player, v - time in seconds of same by duration move of one winner player for somewhat ideal situation when players play moves exactly after certain period of time elapses, m - time in minutes given for each player not including addings for each completed move, s - seconds which added for each completed move. Parameters allowed to be a real decimal values not only integers. Type commands and hit enter: not legal recognized command - for exit from program");
+	fputs(MESSAGE_HELP,stdout);
 }
-void run_command_processor(){}
-int main(int argc,char**args,char**env){
+void run_command_processor(){
 	print_help();
+}
+int main(int argc,char**args,char**env){
 	run_command_processor();
 	return EXIT_SUCCESS;
 }
