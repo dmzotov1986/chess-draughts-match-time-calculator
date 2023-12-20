cb6e690ebbca4361ccea1b9616905360e6884809
dmzotov1986 <152577337+dmzotov1986@users.noreply.github.com>
2023-12-20 07:17:31 +0200

    Update match_calc.c

--- match_calc.c
+++ match_calc.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"settings.h"
-void print_help(){
+void print_help(void){
 	fputs(MESSAGE_HELP,stdout);
 }
-void run_command_processor(){
+void run_command_processor(void){
 	print_help();
 }
 int main(int argc,char**args,char**env){
